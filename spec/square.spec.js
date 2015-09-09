1129c811c1ea08bf8ceb2068e76f9fa7222e29e2
flackend <jared.flack@gmail.com>
2015-09-09 18:40:50 -0400

    Adds square.js and spec

---
+++ spec/square.spec.js
@@ -0,0 +1,11 @@
+var square = require('../src/square.js') ;
+
+describe('square', function() {
+    it('should return the quare of the number it is passed', function() {
+        expect(square(0)).toBe(0 * 0);
+        expect(square(1)).toBe(1 * 1);
+        expect(square(5)).toBe(5 * 5);
+        expect(square(279217897291798792)).toBe(279217897291798792 * 279217897291798792);
+        expect(square(-7)).toBe(-7 * -7);
+    });
+});
