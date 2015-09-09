f222fe63a386b688bb381d02e729515e6a991c44
flackend <jared.flack@gmail.com>
2015-09-09 19:00:11 -0400

    Updates karma.conf.js to use bundle.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js',
+      'dist/bundle.js',
       'spec/**/*[sS]pec.js'
     ],
 
